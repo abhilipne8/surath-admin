742b279474bdf821eca8723c269ae1d223630e91
Abhishek Lipne <abhisheklipne0@gmail.com>
2025-06-02 02:37:13 +0530

    router change for host app

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { HMSRoomProvider } from '@100mslive/react-sdk';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/admin">
     <Provider store={store}>
       <HMSRoomProvider>
         <App />
