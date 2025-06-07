614078e0dbe6e88832849f7d75a3ba2ef8299b72
Abhishek Lipne <abhisheklipne0@gmail.com>
2025-06-07 15:17:33 +0530

    Api changes

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://sorathgaming.fun/api',
 });
 
 // Request Interceptor to Attach Token
